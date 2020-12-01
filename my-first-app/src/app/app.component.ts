d0a78cb49a2b6f2f892e40b179f85da28fd961da
pm1tropoulo <pmitropoulos@outlook.com.gr>
2020-12-01 17:02:38 +0200

    05.059_068
    Chapter 05 Components Data Binding Deep Dive/059-068

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  serverElements = [{type:'server', name: 'TestServer', content: 'Just a  Test !!'}];
+  
+  onServerAdded(serverData: {serverName:string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(serverBpData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverBpData.serverName,
+      content: serverBpData.serverContent
+    });
+  }
+
 }
---
+++ my-first-app/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName:string, serverContent: string}>();
+  @Output() blueprintCreated = new EventEmitter<{serverName:string, serverContent: string}>();
+  // newServerName = '';
+  newServerContent = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer(serverName: HTMLInputElement) {
+    this.serverCreated.emit({
+      serverName: serverName.value, 
+      serverContent: this.newServerContent});
+  }
+
+  onAddBlueprint(serverName: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: serverName.value, 
+      serverContent: this.newServerContent});
+  }
+} 
