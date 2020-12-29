fa34ec589de39897b0c1a9fa37b2d371d91842ed
pm1tropoulo <pmitropoulos@outlook.com.gr>
2020-12-29 12:46:39 +0200

    Subscription
    observables- Subscription intro

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ServersService } from './servers.service';
 
 @Component({
@@ -10,12 +10,14 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService, private router: Router) { }
+  constructor(private serversService: ServersService,
+     private router: Router,
+     private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
   onReload(){
-    this.router.navigate(['/servers']);
+    // this.router.navigate(['/servers']);
   }
 }
